f575a08acb1c98f6e06a7fbf53db0cc24bc6a945
anshikakaushik17 <kaushikanshika2000@gmail.com>
2025-07-19 17:36:58 +0530

    Made some changes for production environment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,11 +11,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-} // Allow requests from the frontend
-));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    } // Allow requests from the frontend
+    ));
+}
+
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter); // Apply rate limiting middleware
@@ -25,7 +30,14 @@ app.use(rateLimiter); // Apply rate limiting middleware
 //     next();
 // })
 
-app.use("/api/notes", notesRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.use("/api/notes", notesRoutes);
+
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
